fa393af7a14856103c3ed4d5230a8843b4f0006f
Janani <75440572+Jananisankar21419@users.noreply.github.com>
2024-08-01 00:40:44 +0530

    Update supabaseClient.mjs

--- src/app/services/supabaseClient.mjs
+++ src/app/services/supabaseClient.mjs
@@ -1,9 +1,8 @@
-// services/supabaseclient.mjs
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-// Replace with your actual Supabase URL and public API key
-const SUPABASE_URL = "https://uzmbevhhtbopnawhrhcr.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6bWJldmhodGJvcG5hd2hyaGNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzMTM0NjMsImV4cCI6MjAzNzg4OTQ2M30.xFmPkoY3kAhjNkshJuIRVSfdymL0QAP6Xgqv3xhK-oc";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
